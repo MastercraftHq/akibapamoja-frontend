53a2bfe2aaa84f456cbeac572da2741d252ad68a
Elai5 <yvettelaine007@gmail.com>
2025-07-27 14:08:50 +0300

    added plus button icon on avator icon

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,7 +1,19 @@
 /** @format */
 
 import profileAvatar from "./profilepicture.jpeg";
+import avator1 from "./avator1.png";
+import avator2 from "./avator2.png";
+import avator3 from "./avator3.png";
+import avator4 from "./avator4.png";
+import avator5 from "./avator5.png";
+import avator6 from "./avator6.png";
 
 export const assets = {
   profileAvatar,
+  avator1,
+  avator2,
+  avator3,
+  avator4,
+  avator5,
+  avator6,
 };
--- src/pages/setprofile/index.tsx
+++ src/pages/setprofile/index.tsx
@@ -1,9 +1,7 @@
 /** @format */
-import { ArrowLeft } from "lucide-react";
-// import { CirclePlus } from "lucide-react";
+import { ArrowLeft, Plus } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-// import Buttons from "../../components/Buttons";
 
 import { assets } from "@/assets/assets";
 import { Link } from "react-router-dom";
@@ -29,44 +27,63 @@ const SetProfile = () => {
         <h2 className="text-xl font-semibold font-geist mb-3">
           Choose Profile picture
         </h2>
-        <div className="w-52 h-52 rounded-full border-1 border-dashed border-gray-400 overflow-hidden flex items-center justify-center">
+        <div className="relative w-52 h-52 rounded-full border border-dashed border-gray-400 flex items-center justify-center">
           <img
             src={assets.profileAvatar}
-            alt="assetsImage"
-            className="w-38 h-38 object-cover"
+            alt="Profile Avatar"
+            className="w-38 h-38 object-cover rounded-full"
           />
+          <button
+            className="absolute top-[20%] -right-4 -translate-x-1/2 -translate-y-1/2 bg-[#4107A5] text-white rounded-full w-9 h-9 flex items-center justify-center shadow-md z-10"
+            aria-label="Add"
+          >
+            <Plus className="w-5 h-5" />
+          </button>
         </div>
+
         <p className="text-xs mt-6">Or choose Akiba Chama avator</p>
         <div className="flex flex-col gap-3 items-center">
           <div className="flex gap-4">
             {" "}
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <div className="bg-[#FFDBDE] p-1 rounded-full inline-block">
+              <Avatar className="w-10 h-10">
+                <AvatarImage src={assets.avator1} alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="bg-[#FFF2B9] p-1 rounded-full inline-block">
+              <Avatar className="w-10 h-10">
+                <AvatarImage src={assets.avator2} alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="bg-[#D1FFF4] p-1 rounded-full inline-block">
+              <Avatar className="w-10 h-10">
+                <AvatarImage src={assets.avator3} alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="bg-[#FFC0B8] p-1 rounded-full inline-block">
+              <Avatar className="w-10 h-10">
+                <AvatarImage src={assets.avator4} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
           </div>
           <div className="flex gap-4">
             {" "}
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <div className="bg-[#FFF2B9] p-1 rounded-full inline-block">
+              <Avatar className="w-10 h-10">
+                <AvatarImage src={assets.avator5} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="bg-[#FFA576] p-1 rounded-full inline-block">
+              <Avatar className="w-10 h-10">
+                <AvatarImage src={assets.avator6} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
           </div>
         </div>
 
